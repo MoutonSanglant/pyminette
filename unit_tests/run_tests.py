4fb7795766f9cc26e2b23221fb1237c93af8f356
MoutonSanglant <defresne.thierry@gmail.com>
2016-12-01 04:42:32 +0100

    fixed file read

--- unit_tests/run_tests.py
+++ unit_tests/run_tests.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
+# coding: utf8
 
 import sys;
 from tests.tests import *;
+
+FLAG_COLOR = 0x1;
+FLAG_FULL = 0x2;
+
+flags = 0;
+
+for arg in sys.argv[1:]:
+    if arg[0] == '-':
+        if 'c' in arg:
+            flags |= FLAG_COLOR;
+        if 'f' in arg:
+            flags |= FLAG_FULL;
 
 test_length();
--- unit_tests/tests/tests.py
+++ unit_tests/tests/tests.py
@@ -1,11 +1,13 @@
+# coding: utf8
 
+import os;
 import filecmp;
 from subprocess import call;
 
 def compare_file(filename, res):
-    f = open('tests/results/' + filename, 'r');
+    f = open('results/' + filename, 'r');
     exp = f.read();
-    if cmp(res, exp):
+    if res != exp:
         print("\033[31m[-]\033[0m " + filename);
         print('expected:');
         print(exp);
@@ -13,17 +15,17 @@ def compare_file(filename, res):
         print(res);
     else:
         print("\033[32m[+]\033[0m " + filename);
-    
 
 def test_file(filename):
-    with open('tmp', 'r+') as outfile:
-#        print('inputs/' + filename + '.c');
-#        quit();
-        call(['../pyminette.py', 'tests/inputs/' + filename + '.c'], stdout=outfile);
+    with open('tmp', 'w+') as outfile:
+        call(['../pyminette.py', 'inputs/' + filename + '.c'], stdout=outfile);
         outfile.seek(0);
         compare_file(filename, outfile.read());
-#        print(outfile.read());
+        os.remove('tmp');
 
 def test_length():
     test_file('valid_length');
     test_file('invalid_length');
+
+def full_test():
+    test_length();
